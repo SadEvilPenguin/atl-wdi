5d107836a2ddd412f530baf0f60c7002201cd69f
SadEvilPenguin <mears.chris@gmail.com>
2017-09-07 15:29:45 -0400

    event update

--- labs/javaScript/in-class-click-events/script.js
+++ labs/javaScript/in-class-click-events/script.js
@@ -1,2 +1,35 @@
 window.onload = function() {
+    const button = document.querySelectorAll("button");
+    const text = "A click event is essentially tying a function (as a callback) to an element and specifying what action needs to happen to trigger or call/invoke that function (callback).";
+    const p = document.createElement("p");
+
+
+    function createAlert() {
+        alert("you have clicked button one");
+    }
+
+    function addText() {
+        p.innerText = text;
+        document.body.appendChild(p);
+    }
+
+    function removeText() {
+        const removeP = document.querySelector("p");
+        document.body.removeChild(removeP);
+    }
+
+    //button[0].addEventListener("click", createAlert);
+    //button[1].addEventListener("click", addText);
+    //button[2].addEventListener("click", removeText);
+    button.forEach((element) => {
+        element.addEventListener("click", function() {
+            if (element === button[0]) {
+                createAlert();
+            } else if (element === button[1]) {
+                addText();
+            } else if (element === button[2]) {
+                removeText();
+            }
+        });
+    });
 }
