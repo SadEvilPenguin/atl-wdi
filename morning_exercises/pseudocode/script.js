e9aff3a9eda66bf03fb098b026c2bbfab0158aee
SadEvilPenguin <mears.chris@gmail.com>
2017-09-07 09:17:13 -0400

    pseudocode workthrough

--- morning_exercises/pseudocode/script.js
+++ morning_exercises/pseudocode/script.js
@@ -2,28 +2,37 @@
 // Problem 1:
 // Barrels O' RUM
 
-var barrels = function(small, large, total){
+var barrels = function(small, large, total) {
+    let smallPrice = 60;
+    total = total - (60 * small);
+    return total / large;
 };
 
 //*************************
 // Problem 2:
 // Sailing the Seas
 
-var shipFuelCost = function(fuelPrice, milesPerGallon){
-  //your code here
+var shipFuelCost = function(fuelPrice, milesPerGallon) {
+    let curcumference = 24901;
+    let totalGallons = 24901 / milesPerGallon;
+    return totalGallons * fuelPrice;
 };
 
 //*************************
 // Problem 3:
 // GROG
 
-var calcFruitJuice = function(a, b, c, d){
-  //your code here
+var calcFruitJuice = function(a, b, c, d) {
+    let costco = a * (b / 100);
+    let kirkland = c * (d / 100);
+    let juiceTotal = costco + kirkland;
+    let total = a + c;
+    return juiceTotal / total;
 };
 
 //DO NOT EDIT BELOW THIS LINE//
 module.exports = {
-  barrels: barrels,
-  shipFuelCost: shipFuelCost,
-  calcFruitJuice: calcFruitJuice
+    barrels: barrels,
+    shipFuelCost: shipFuelCost,
+    calcFruitJuice: calcFruitJuice
 };
