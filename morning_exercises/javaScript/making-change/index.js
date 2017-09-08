c7618b3d34dc6eb04068b7db9262f3a3abaf5ee0
SadEvilPenguin <mears.chris@gmail.com>
2017-09-08 10:49:17 -0400

    morning exercise make change work

--- morning_exercises/javaScript/making-change/index.js
+++ morning_exercises/javaScript/making-change/index.js
@@ -4,11 +4,46 @@
 "use strict";
 /// DO NOT EDIT ABOVE THIS LINE ///
 
-var makeChange = function(price, payment) {
-  // Your Code Here
-};
+function makeChange(price, payment, denominations) {
+    let change = parseInt((payment - price) * 100);
+    console.log(change);
+    //create Array and remainder after coin
+    let coinArray = [];
+    let coin = 0;
+    let remainder = 0;
+    if ()
+        denominations.forEach(function(element) {
+            coin = Math.trunc(change / element);
+            coinArray.push(coin);
+            change = change % element;
+        });
 
+    if (change !== 0) {
+        console.log(`please make sure to add penny at end of coins array otherwise you will have ${change} remaining`)
+    }
+    return coinArray;
+}
+
+
+/* var makeChange = function(price, payment) {
+    let change = (payment - price)*100;
+    if (change <= 0) {
+      return [0,0,0,0];
+    } else {
+      const quarters = Math.floor(change / 25);
+      chang = change % 25;
+      const dimes = Math.floor(change / 10);
+      chang = change % 10;
+      const nickels = Math.floor(change / 5);
+      chang = change % 5;
+      const pennies = Math.floor(change / 1);
+      chang = change % 1;
+      return [quarters, dimes, nickels, pennies];
+    }
+}
+
+*/
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
-  makeChange: makeChange
+    makeChange: makeChange
 };
