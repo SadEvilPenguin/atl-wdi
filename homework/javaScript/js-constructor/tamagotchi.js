cd1d59f504b07eed86a093f78bf54bfb4d61c30b
SadEvilPenguin <mears.chris@gmail.com>
2017-09-24 11:38:47 -0400

    added more DOM manipulation and randomize timeout events plus a stop function for hiting zero on any object property

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -12,38 +12,59 @@ class Tamagotchi {
     }
     cry() {
         this.foodInTummy--;
-        document.getElementById(this.name).innerHTML = `WAHH!!! Food Left: ${this.foodInTummy}`;
+        document.getElementById(this.name).innerHTML = `WAHH!!! -Food Left: ${this.foodInTummy}`;
     }
     puke() {
         this.health--;
-        document.getElementById(this.name).innerHTML = `BlAHH!!! Health left: ${this.health}`;
+        document.getElementById(this.name).innerHTML = `BlAHH!!! -Health left: ${this.health}`;
     }
     yawn() {
         this.restedness--;
-        document.getElementById(this.name).innerHTML = `has current restedness of: ${this.restedness}`;
+        document.getElementById(this.name).innerHTML = `Yawn!!! -Current restedness of: ${this.restedness}`;
     }
-
     start() {
         let self = this;
-        this.hungerTimer = setInterval(() => { self.cry() }, 6000);
-        this.yawnTimer = setInterval(() => { self.yawn() }, 10000);
-        this.sickTimer = setInterval(() => { self.puke() }, 20000);
+        this.hungerTimer = setTimeout(() => {
+            self.cry();
+            self.start()
+        }, getRandom(5000, 15000));
+        this.yawnTimer = setTimeout(() => {
+            self.yawn();
+            self.start()
+        }, getRandom(15000, 30000));
+        this.sickTimer = setTimeout(() => {
+            self.puke();
+            self.start()
+        }, getRandom(20000, 30000));
     }
     stop() {
-        clearInterval(this.hungerTimer);
-        clearInterval(this.yawnTimer);
-        clearInterval(this.sickTimer);
+        this.check = setInterval(() => {
+            if (this.foodInTummy === 0 || this.restedness === 0 || this.health === 0) {
+                clearInterval(this.hungerTimer);
+                clearInterval(this.yawnTimer);
+                clearInterval(this.sickTimer);
+                document.getElementById(this.name).innerHTML = `${this.name} has died!`;
+                document.getElementsByClassName(this.name)[0].childNodes[0].setAttribute("src", "./img/tama_dead.gif");
+                clearInterval(this.check);
+            }
+        }, 1000);
     }
 };
 
-function createTama(id) {
+function getRandom(min, max) {
+    let random = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(random);
+    return random;
+}
+
+function createTama(id, type) {
     let tama = document.createElement('div');
     tama.setAttribute("class", id);
     document.getElementById('tama').appendChild(tama);
     let pic = document.createElement('img');
     pic.setAttribute("src", "./img/tama.gif");
     let name = document.createElement('p');
-    name.appendChild(document.createTextNode(id));
+    name.appendChild(document.createTextNode(`${id} the ${type}`));
     tama.appendChild(pic);
     tama.appendChild(name);
     let tamaSay = document.createElement('div');
@@ -56,11 +77,13 @@ function createTama(id) {
 
 };
 
+
 //create new Tamagotchis
 let create = (name, creature) => {
     this[name] = new Tamagotchi(name, creature);
     this[name].start();
-    createTama(name);
+    this[name].stop();
+    createTama(name, creature);
 }
 
 //test out your Tamagotchies below via console.logs
