91db31046722876f5bee0c057ef986700c935739
SadEvilPenguin <mears.chris@gmail.com>
2017-09-26 17:59:59 -0400

    successfully created donut_store database and created donut schema

--- homework/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,35 @@
 //requirements: require mongoose
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+mongoose.Promise = global.Promise;
 
 //create your donut schema:
+const donutSchema = new Schema({
+    //write your schema fields here
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    qty: {
+        type: Number,
+    },
 
+});
 
 //export your donut with module.exports()
+const DonutModel = mongoose.model("Donut", donutSchema);
+
+module.exports = {
+    DonutModel: DonutModel
+}
