2fdcea2562c6edf144f84cee93c8693b39e7bcd4
SadEvilPenguin <mears.chris@gmail.com>
2017-09-27 13:55:11 -0400

    added promises.js

---
+++ homework/javaScript/promise-it-wont-hurt/program.js
@@ -0,0 +1,20 @@
+//Warm Up
+
+// setTimeout(() => {
+//     console.log('TIMED OUT!')
+// }, 300);
+
+//Fulfill a promise
+// const promise = new Promise(function(fulfill, reject) {
+//     // Your solution here
+
+//     setTimeout(() => {
+//         fulfill('FULFILLED!')
+//     }, 300)
+// });
+
+// promise.then((message) => {
+//     console.log(message)
+// })
+
+//Reject a promise
