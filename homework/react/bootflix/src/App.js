bfd56488c42eadffaafcb0ff212abd9e2a00a746
SadEvilPenguin <mears.chris@gmail.com>
2017-10-10 21:15:39 -0400

    updated movie component to be dynamic

--- homework/react/bootflix/src/App.js
+++ homework/react/bootflix/src/App.js
@@ -3,22 +3,61 @@ import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
 import example from './omdbExample.json'
+import axios from 'axios'
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      movie: example
+      movie: example,
+      searchTitle: '',
+      searchId: '',
+      img: '',
     }
   }
 
-  //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
-  _searchByTitle = () => {
-    console.log("Search by Title");
+  handleSearchTitle = (event) => {
+    const searchTitle = event.target.value;
+    this.setState({searchTitle});
   }
 
-  _searchById = () => {
-    console.log("Search by ID");
+  handleSearchId = (event) => {
+    const searchId = event.target.value;
+    this.setState({searchId});
+  }
+
+  //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
+  searchByTitle = (event) => {
+    event.preventDefault()
+    console.log(this.state.searchTitle)
+    axios.get("http://www.omdbapi.com/", {
+      params: {
+        t: this.state.searchTitle,
+        apikey: "d31f1a94"
+      }
+    })
+    .then(res => {
+      const movie = res.data
+      this.setState({movie})
+    }).catch((error) => {
+      console.error("Error: ", error);
+  })
+  }
+
+  searchById = (event) => {
+    event.preventDefault()
+    axios.get("http://www.omdbapi.com/", {
+      params: {
+        i: this.state.searchId,
+        apikey: "d31f1a94"
+      }
+    })
+    .then(res => {
+      const movie = res.data
+      this.setState({movie})
+    }).catch((error) => {
+      console.error("Error: ", error);
+  })
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
@@ -26,8 +65,12 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Search />
-        <Movie />
+        <Search 
+        searchByTitle={this.searchByTitle} 
+        searchById={this.searchById}
+        handleSearchTitle={this.handleSearchTitle}
+        handleSearchId={this.handleSearchId} />
+        <Movie movie={this.state.movie} />
       </div>
     );
   }
