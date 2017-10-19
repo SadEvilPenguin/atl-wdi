a08302d99fe45e961f633a2d68c3226b7b49bf57
SadEvilPenguin <mears.chris@gmail.com>
2017-10-19 19:44:52 -0400

    updated seeds

--- homework/sql/realty/realty_seed.sql
+++ homework/sql/realty/realty_seed.sql
@@ -1,8 +1,8 @@
-INSERT INTO apartments (apartment_number, number_of_bedrooms, address, tenant, occupied_status, square_footage, price) VALUES ('3A', 2, '1231 Street St City,ST 00000', 'Jeff Jefferson', 'true', 800, 800)
-INSERT INTO apartments (apartment_number, number_of_bedrooms, address, tenant, occupied_status, square_footage, price) VALUES ('404', 3, '1321 Lane LN City,ST 99999', 'Tom Tomson', 'true', 1200, 1200)
+INSERT INTO apartments (apartment_number, number_of_bedrooms, address, tenant, occupied_status, square_footage, price) VALUES ('3A', 2, '1231 Street St City,ST 00000', 'Jeff Jefferson', 'true', 800, 800);
+INSERT INTO apartments (apartment_number, number_of_bedrooms, address, tenant, occupied_status, square_footage, price) VALUES ('404', 3, '1321 Lane LN City,ST 99999', 'Tom Tomson', 'true', 1200, 1200);
 
-INSERT INTO offices (office_number, number_of_floors, square_footage, number_of_cubicles, number_of_bathrooms, address, company_name, occuiped_status, price) VALUES ('suite 1', 99, 1000000, 1000, 5, '123 Work here seat,CT 303030', 'Turtle Bell', 'true', 100.12)
-INSERT INTO offices (office_number, number_of_floors, square_footage, number_of_cubicles, number_of_bathrooms, address, company_name, occuiped_status, price) VALUES ('suite 2', 2, 10000, 100, 2, '123 Dont Work here seat,CT 303030', 'Not Turtle Bell', 'true', 200.12)
+INSERT INTO offices (office_number, number_of_floors, square_footage, number_of_cubicles, number_of_bathrooms, address, company_name, occuiped_status, price) VALUES ('suite 1', 99, 1000000, 1000, 5, '123 Work here seat,CT 303030', 'Turtle Bell', 'true', 100.12);
+INSERT INTO offices (office_number, number_of_floors, square_footage, number_of_cubicles, number_of_bathrooms, address, company_name, occuiped_status, price) VALUES ('suite 2', 2, 10000, 100, 2, '123 Dont Work here seat,CT 303030', 'Not Turtle Bell', 'true', 200.12);
 
-INSERT INTO storefronts (address, occuiped_status, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('123 storefront city,st 23092309', 'true', 1200.12, 'true', 950, 'James Royals Pants', 'true')
-INSERT INTO storefronts (address, occuiped_status, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('123  bad storefront city,st 23092', 'false', 1900.12, 'false', 1950, 'Me')
+INSERT INTO storefronts (address, occuiped_status, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('123 storefront city,st 23092309', 'true', 1200.12, 'true', 950, 'James Royals Pants', 'true');
+INSERT INTO storefronts (address, occuiped_status, price, kitchen, square_footage, owner, outdoor_seating) VALUES ('123  bad storefront city,st 23092', 'false', 1900.12, 'false', 1950, 'Me');
