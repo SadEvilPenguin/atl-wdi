5d75d3f3bd5d1f1f2b6038dc8027d4b1df6ccb41
SadEvilPenguin <mears.chris@gmail.com>
2017-10-19 17:43:14 -0400

    CARMEN SANDIEGO - I found Carmen Sandiego

--- homework/sql/carmen-sandiego/starter-code/find_carmen.sql
+++ homework/sql/carmen-sandiego/starter-code/find_carmen.sql
@@ -10,6 +10,19 @@ SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
 SELECT country.name, country.code, country.region  From country INNER JOIN countrylanguage AS lang ON country.code=lang.countrycode WHERE lang.language = 'Italian' AND lang.percentage=100
 --San Marino, SMR--
 
---Answer To CLue 4--
+--Answer To Clue 4--
 SELECT * FROM city WHERE countrycode='SMR';
 --Serravalle--
+
+--Answer To Clue 5--
+SELECT * FROM city WHERE name LIKE 'Serra%';
+--SERRA, BRA--
+
+--Answer To Clue 6--
+SELECT capital FROM country WHERE code='BRA';
+SELECT name FROM country WHERE id=211;
+--Brasilia--
+
+--ANSWER TO Clue 7, 8--
+SELECT * FROM city WHERE population=91084;
+--Santa Monica--
