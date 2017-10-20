3f6396fbd930c960be391f1ad26b4127d34b18b2
SadEvilPenguin <mears.chris@gmail.com>
2017-10-20 12:15:46 -0400

    finished lab

--- labs/sql/library_sql/advanced_queries.sql
+++ labs/sql/library_sql/advanced_queries.sql
@@ -2,21 +2,22 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-    SELECT * FROM authors INNER JOIN books ON authors.id=books.author_id WHERE authors.name='George R.R. Martin';
+    SELECT * FROM authors JOIN books ON authors.id=books.author_id WHERE authors.name='George R.R. Martin';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-    SELECT * FROM authors INNER JOIN books ON authors.id=books.author_id WHERE authors.name='Milan Kundera';
+    SELECT * FROM authors JOIN books ON authors.id=books.author_id WHERE authors.name='Milan Kundera';
 -- Find all books written by an author from China or the UK.
-    SELECT books.title FROM authors INNER JOIN books ON authors.id=books.author_id WHERE authors.nationality='China' OR authors.nationality='United Kingdom';
+    SELECT title FROM authors JOIN books ON authors.id=books.author_id WHERE nationality='China' OR nationality='United Kingdom';
 -- Find out how many books Albert Camus wrote.
-    SELECT COUNT (books.title) FROM authors INNER JOIN books ON authors.id=books.author_id WHERE authors.name='Albert Camus';
+    SELECT COUNT (title) FROM authors JOIN books ON authors.id=books.author_id WHERE name='Albert Camus';
 -- Find out how many books were written by US authors.
-    SELECT COUNT (books.title) FROM authors INNER JOIN books ON authors.id=books.author_id WHERE authors.nationality='United States';
+    SELECT COUNT (title) FROM authors JOIN books ON authors.id=books.author_id WHERE nationality='United States';
 -- Find all books written after 1930 by authors from Argentina.
-    SELECT books.title FROM authors INNER JOIN books ON authors.id=books.author_id WHERE authors.nationality='Argentina' AND books.publication_date > 1930;
+    SELECT title FROM authors JOIN books ON authors.id=books.author_id WHERE nationality='Argentina' AND publication_date > 1930;
 -- Find all books written before 1980 by authors not from the US.
-    SELECT books.title FROM authors INNER JOIN books ON authors.id=books.author_id WHERE NOT authors.nationality='United States' AND books.publication_date < 1980;
+    SELECT title FROM authors JOIN books ON authors.id=books.author_id WHERE NOT nationality='United States' AND publication_date < 1980;
 -- Find all authors whose names start with 'J'.
-    SELECT * FROM authors WHERE name Like '%J'
+    SELECT * FROM authors WHERE name LIKE 'J%';
 -- Find all books whose titles contain 'the'.
-
+    SELECT * FROM books WHERE title LIKE '%the%';
 -- Find all authors who have written books with that start with the letter 'N'.
+    SELECT name, title FROM authors JOIN books ON authors.id = books.author_id WHERE title LIKE 'N%';
