83ad9a191bd758b6bd617c8ae948795894a78c04
SadEvilPenguin <mears.chris@gmail.com>
2017-10-23 19:25:37 -0400

    finished ruby ninja project

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -1,115 +1,115 @@
 #--Challenge 1 --
-# a = true
-# b = false
-# puts a == b
-# puts a != b
+a = true
+b = false
+puts a == b
+puts a != b
 
 #--Challenge 2 --
-# my_choosing = nil
-# p my_choosing
-# puts "#{my_choosing} was here"
-# print my_choosing
-# puts my_choosing.class
+my_choosing = nil
+p my_choosing
+puts "#{my_choosing} was here"
+print my_choosing
+puts my_choosing.class
 
 #--Challenge 3--
-# d = 9
-# puts d
-# e = 3.3
-# puts e
-# d = e
-# puts d
+d = 9
+puts d
+e = 3.3
+puts e
+d = e
+puts d
 
 
 #--Challenge 4--
-# christine = "Hi, I'm Christine"
-# likesTo = "long walks on the beach, kinda."
-# puts christine + " and I like " + likesTo
-# g = "2"
-# g_to_number = g.to_i
-# puts g.to_i + g_to_number
+christine = "Hi, I'm Christine"
+likesTo = "long walks on the beach, kinda."
+puts christine + " and I like " + likesTo
+g = "2"
+g_to_number = g.to_i
+puts g.to_i + g_to_number
 
 #--Challenge 5 --
-# puts "Input an Integer"
-# number = gets.chomp.to_i
-# puts number * 2
+puts "Input an Integer"
+number = gets.chomp.to_i
+puts number * 2
 
-# puts "What is your name?"
-# name = gets.chomp
-# puts "What do you like to do?"
-# favorite = gets.chomp
+puts "What is your name?"
+name = gets.chomp
+puts "What do you like to do?"
+favorite = gets.chomp
 
-# puts "#{name} like to #{favorite}"
+puts "#{name} like to #{favorite}"
 
 #--Challenge 6 --
-# myFirstArray = []
-# myFirstArray = ["one", "Two", "Three", "Four", "Five", 1, 2, 3, 4]
-# puts myFirstArray.last
-# myFirstArray[1] = "Joe"
-# myFirstArray[2] = 3
-# myFirstArray.push(true)
-# puts myFirstArray[3].class
+myFirstArray = []
+myFirstArray = ["one", "Two", "Three", "Four", "Five", 1, 2, 3, 4]
+puts myFirstArray.last
+myFirstArray[1] = "Joe"
+myFirstArray[2] = 3
+myFirstArray.push(true)
+puts myFirstArray[3].class
 
 #--Challenge 7 --
-# myFavoriteAnimals = {
-#     "Edgar" => "Donkey",
-#     "Chris" => "Turtle",
-#     "James" => "Cat",
-#     "Aaron" => "Dog",
-#     "Ericka" => "Peacock"
-# }
+myFavoriteAnimals = {
+    "Edgar" => "Donkey",
+    "Chris" => "Turtle",
+    "James" => "Cat",
+    "Aaron" => "Dog",
+    "Ericka" => "Peacock"
+}
 
-# myFavoriteAnimals["Edgar"] = "Bear"
+myFavoriteAnimals["Edgar"] = "Bear"
 
-# favoriteMovie = {}
-# favoriteMovie[:movie] = "Back to the future"
+favoriteMovie = {}
+favoriteMovie[:movie] = "Back to the future"
 
-# puts myFavoriteAnimals
-# puts favoriteMovie
+puts myFavoriteAnimals
+puts favoriteMovie
 
 #--Challenge 8 --
-# firstRange = (1..10)
-# firstRange = firstRange.to_a
+firstRange = (1..10)
+firstRange = firstRange.to_a
 
-# secondRange = (1..1000)
-# secondRange = secondRange.to_a
+secondRange = (1..1000)
+secondRange = secondRange.to_a
 
-# for number in firstRange
-#     number = number.to_s
-#     p number
-# end
+for number in firstRange
+    number = number.to_s
+    p number
+end
 
-# x = 0
-# while x < 50 do
-#     puts secondRange[x]
-#     x += 1
-# end
-# new_arr = []
-# firstRange.each do |number|
-#     if number.odd?
-#         new_arr.push(number * 2)
-#     end
-# end
-# puts new_arr
+x = 0
+while x < 50 do
+    puts secondRange[x]
+    x += 1
+end
+new_arr = []
+firstRange.each do |number|
+    if number.odd?
+        new_arr.push(number * 2)
+    end
+end
+puts new_arr
 
-# another_arr = firstRange.map do |num|
-#     if num.even?
-#         num.to_s
-#     else
-#         num
-#     end
-# end
-# p another_arr
+another_arr = firstRange.map do |num|
+    if num.even?
+        num.to_s
+    else
+        num
+    end
+end
+p another_arr
 
 #--Challenge 9--
-# a = (1..1000).to_a
-# sum_arr = []
-# a.each do |num|
-#     if num % 3 == 0 || num % 5 == 0
-#         sum_arr.push(num)
-#     end
-# end
-# total = sum_arr.sum
-# puts total
+a = (1..1000).to_a
+sum_arr = []
+a.each do |num|
+    if num % 3 == 0 || num % 5 == 0
+        sum_arr.push(num)
+    end
+end
+total = sum_arr.sum
+puts total
 
 #--Final Challenge--
 def check_prime?(number)
@@ -122,6 +122,5 @@ def check_prime?(number)
 end
 
 def get_primes(number)
-    arr = []
-    arr.push((1..number).select{|n| check_prime? n})
+    return (1..number).select{|n| check_prime? n}
 end
