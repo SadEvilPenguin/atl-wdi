daa9db154b6a810c123aca43c5570bd7d8444a6d
SadEvilPenguin <mears.chris@gmail.com>
2017-10-23 17:21:19 -0400

    3. Integers Skills

--- homework/ruby/ninja_skills.rb
+++ homework/ruby/ninja_skills.rb
@@ -4,9 +4,20 @@ b = false
 puts a == b
 puts a != b
 
-
+#--Challenge 2 --
 my_choosing = nil
 p my_choosing
-puts my_choosing.to_s + " was here"
+puts "#{my_choosing} was here"
 print my_choosing
 puts my_choosing.class
+
+#--Challenge 3--
+d = 9
+puts d
+e = 3.3
+puts e
+d = e
+puts d
+
+
+#--Challenge 4--
